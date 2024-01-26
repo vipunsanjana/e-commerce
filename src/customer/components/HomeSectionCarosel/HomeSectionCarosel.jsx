f1dff07f2363434e98faab9d149acd87d96a0e0c
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2024-01-26 18:27:25 +0530

    will add home data

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard'
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const HomeSectionCarosel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
 
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 4 },
-    };
+  const slidePrev = () => {
+    setActiveIndex(activeIndex-1);
+  }
+  const slideNext = () => {
+    setActiveIndex(activeIndex+1);
+  }
 
-    const items = [1,1,1,1,1].map((item)=><HomeSectionCard />)
+  const syncActiveIndex = ({item}) => {
+    setActiveIndex(item);
+  }
+
+  const items = [1, 1, 1, 1, 1,1,1,1,1,1].map((item) => <HomeSectionCard />);
 
   return (
-    <div>
-
-                <AliceCarousel
+    <div className="border">
+      <div className="relative p-5 ">
+      <AliceCarousel
         items={items}
         disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
         infinite
+        disableDotsControls
         responsive={responsive}
-        />
-    </div>
-  )
-}
+        onSlideChanged={syncActiveIndex}
+        activeIndex={activeIndex}
+      />
+      {activeIndex !== items.length-5 &&
+        <Button onClick={slideNext} variant="contained" className="z-50 bg-white" sx={{position:'absolute',top:'8rem',right:'0rem', transform:'translateX(50%) rotate(90deg)', bgcolor:"white"}} aria-label="next">
+          <ArrowBackIosIcon sx={{transform:"rotate(90deg)",color:"black"}} />
+        </Button>
+      }  
 
-export default HomeSectionCarosel
+      <Button onClick={slidePrev} variant="contained" className="z-50 bg-white" sx={{position:'absolute',top:'8rem',left:'0rem', transform:'translateX(-50%) rotate(-90deg)', bgcolor:"white"}} aria-label="next">
+        <ArrowBackIosIcon sx={{transform:"rotate(90deg)",color:"black"}} />
+      </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
